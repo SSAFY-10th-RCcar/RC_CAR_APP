8255003fc07b0d0948fc9d1dfb8ccf8417d5370a
SeungwooKim <soccer98989@naver.com>
2023-11-23 12:04:45 +0000

    Add mobile option

--- RC_CAR_APP.py
+++ RC_CAR_APP.py
@@ -10,6 +10,8 @@ from time import sleep
 import datetime
 import dotenv
 import os
+import paho.mqtt.client as mqtt
+
 dotenv_file = dotenv.find_dotenv()
 dotenv.load_dotenv(dotenv_file)
 
@@ -64,7 +66,7 @@ def sensing():
     num6 = round(accel["z"], 3)
     # print("acc = ", acc, "m/s^2")
 
-    if not -0.1 < acc < 0.1:
+    if acc > 0.08:
         velo += acc
 
     if speed == 0:
@@ -88,14 +90,13 @@ def sensing():
 def DC():
     global W, S, speed
     lock.acquire()
+    #
     if W == 1:  # 가속
         if speed < 200:
             speed += 20
-
     elif S == 1:  # 감속
         if speed > -200:
             speed -= 20
-
     else:  # 0으로
         if speed > 0:
             speed -= 20
@@ -122,11 +123,11 @@ def servo():
     global A, D
     lock.acquire()
 
-    if A == 1:
+    if A == 1:  # Left
         pwm.setPWM(0, 0, 250)
-    elif D == 1:
+    elif D == 1:  # Right
         pwm.setPWM(0, 0, 420)
-    else:
+    else:  # Middle
         pwm.setPWM(0, 0, 355)
     lock.release()
 
@@ -135,7 +136,83 @@ def servo():
     timer4.start()
 
 
-if __name__=="__main__":
+# def on_message(client, userdata, message):
+#     global X, Y
+#     command = message.payload.decode()
+#     X, Y = command.split()
+#     X = round(X)
+#     Y = round(Y)
+#     print("X=", X, ", Y=", Y)
+
+
+def mqtt_subscriber(broker_address, topic):
+    client = mqtt.Client()
+
+    def on_connect(client, userdata, flags, rc):
+        print("Connected with result code " + str(rc))
+        client.subscribe(topic)
+
+    def on_message(client, userdata, message):
+        global X, Y
+        command = message.payload.decode()
+        X, Y = map(float, command.split())
+        print("X=", X, ", Y=", Y)
+        if 95 <= X <= 155:
+            X = 125
+
+    client.on_message = on_message
+    client.on_connect = on_connect
+
+    client.connect(broker_address, 1883, 60)
+    client.loop_start()  # 별도 스레드에서 loop_forever() 실행
+
+    # while True:
+    #     sleep(0.05)
+    # global timer
+    # timer = Timer(0.05, lambda: client.loop_stop())
+    # timer.start()
+
+
+def DC_phone():
+    global Y
+    lock.acquire()
+
+    if Y < 125:  # 전진
+        myMotor.setSpeed(250)
+        myMotor.run(Raspi_MotorHAT.BACKWARD)
+    elif Y > 125:  # 후진
+        myMotor.setSpeed(250)
+        myMotor.run(Raspi_MotorHAT.FORWARD)
+    elif Y == 125:
+        myMotor.run(Raspi_MotorHAT.RELEASE)
+
+    lock.release()
+
+    global timer3
+    timer3 = Timer(0.05, DC_phone)
+    timer3.start()
+
+
+def servo_phone():
+    global X
+    lock.acquire()
+
+    if X < 125:  # Left
+        pwm.setPWM(0, 0, 250)
+    elif X > 125:  # Right
+        pwm.setPWM(0, 0, 420)
+    elif X == 125:  # Middle
+        pwm.setPWM(0, 0, 355)
+
+    lock.release()
+
+    global timer3
+    timer4 = Timer(0.05, servo_phone)
+    timer4.start()
+
+
+# Main
+if __name__ == "__main__":
     # init
     db = mysql.connector.connect(
         host=os.environ["DB_HOST"],
@@ -144,37 +221,67 @@ if __name__=="__main__":
         database=os.environ["DB_SCHEMA"],
         auth_plugin=os.environ["AUTH_PLUGIN"],
     )
+
+    # DB data receive
     cur = db.cursor()
     ready = None
-    timer = None
 
+    # DC motor
     mh = Raspi_MotorHAT(addr=0x6F)
     myMotor = mh.getMotor(2)
+
+    # Servo motor
     pwm = PWM(0x6F)
     pwm.setPWMFreq(60)
+    lock = Lock()  # Mutex
 
-    sense = SenseHat()
-    timer2 = None
-    lock = Lock()
-    timer3 = None
-    timer4 = None
-    speed = 0
-    velo = 0
+    if sys.argv[1] == "keyboard":
+        # Thread Timer
+        timer = None  # polling
+        timer2 = None  # sensing
+        timer3 = None  # DC
+        timer4 = None  # servo
 
-    W, A, S, D = 0, 0, 0, 0
+        speed = 0  # Motor speed
+        velo = 0  # real speed (m/s)
+        W, A, S, D = 0, 0, 0, 0  # Control key
+        sense = SenseHat()  # SenseHat
 
-    signal.signal(signal.SIGINT, closeDB)
-    polling()
-    sensing()
-    DC()
-    servo()
+        # Thread
+        signal.signal(signal.SIGINT, closeDB)  # Finish
+        polling()  # Data receive
+        sensing()  # Data transmit
+        DC()
+        servo()
 
-    # main thread
-    while True:
-        sleep(0.1)
-        if ready == None:
-            continue
+        # Main thread
+        while True:
+            sleep(0.05)
+            if ready == None:
+                continue
 
-        W, A, S, D = ready
-        ready = None
+            W, A, S, D = ready
+            ready = None
 
+    elif sys.argv[1] == "phone":
+        # Thread Timer
+        timer = None  # polling_phone
+        # timer2 = None  # sensing
+        timer3 = None  # DC_phone
+        timer4 = None  # servo_phone
+
+        speed = 0  # Motor speed
+        velo = 0  # real speed (m/s)
+        broker_address = os.environ["BROKER_ADDR"]  # 라즈베리파이 #2의 IP 주소
+        topic = os.environ["MQTT_TOPIC"]  # 토픽
+        X, Y = 125, 125
+
+        # Thread
+        signal.signal(signal.SIGINT, closeDB)  # Finish
+        mqtt_subscriber(broker_address, topic)
+        # sensing()
+        DC_phone()
+        servo_phone()
+
+        while True:
+            sleep(0.05)
